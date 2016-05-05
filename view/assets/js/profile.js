78bbdf0a59904eb59bac44a683cef7e812f4baca
Syifa Khairunnisa <syifa.khairunnisa21@ui.ac.id>
2016-05-05 13:03:46 +0700

    penawaran - tentang, done. proyek - kolaborasi, done.

--- view/assets/js/profile.js
+++ view/assets/js/profile.js
@@ -69,6 +69,7 @@ $(document).ready(function() {
 		function () {
 			var id = $(this).get(0).id;
 			$("#"+id+" > .panel-edit").show();
+			$("#"+id+" > div > div > div > .panel-edit").show();
 		}, 
 		function () {
 			$(".panel-edit").hide();
@@ -96,6 +97,13 @@ $(document).ready(function() {
     hoverPanel("#panel-solusi");
     hoverPanel("#panel-sasaran");
     hoverPanel("#panel-timeline");
+
+    hoverPanel("#panel-eligibilitas");
+    hoverPanel("#panel-faq");
+
+    hoverPanel("[id^=panel-dana-]");
+    hoverPanel("[id^=panel-sdm-]");
+    hoverPanel("[id^=panel-tekno-]");
     /* END OF HOVER PANEL */
 
 
@@ -136,6 +144,8 @@ $(document).ready(function() {
 
 	function lihatLebihLanjut(id){
 		$(".panel-body > #"+id+" > .lebih-lanjut").slideToggle();
+
+		$(".panel-body > #"+id+" > ul > .lebih-lanjut").slideToggle();
 	}
     /* END OF LIHAT LEBIH LANJUT */
 
@@ -194,5 +204,103 @@ $(document).ready(function() {
 		}
 	});
     /* END OF HOVER ENDORSE BUTTON */
+	$("[id^=id-penawaran-item]").hover(
+		function(){
+			$(this).addClass("hovered");
+		},
+		function(){
+			$(this).removeClass("hovered");
+	});
 
+	$("[id^=id-penawaran-item]").click(function(event) {
+		event.preventDefault();
+		if($("[id^=id-penawaran-item]").hasClass("selected")){
+			$($("[id^=id-penawaran-item]")).removeClass("selected");
+			$(this).addClass("selected");
+		}
+		else{
+			$(this).addClass("selected");
+		}
+	});
+
+
+    /* FORM PROYEK */
+    $("[id^=bantu-]").hide();
+
+    $(".form-cancel").click(function(event) {
+    	$(this).parent().parent().parent().parent().parent().hide();
+    });
+
+    	/* DANA */
+    $("[id^=btn-dana-]").click(function(event) {
+    	$(this).parent().siblings("[id^=bantu-dana-]").show();
+    });
+	$("[id^=dana-meetmail-]").hide();
+	$("[id^=li-dana-penawaran-]").click(function(event) {
+		$(this).addClass("active");
+		$("[id^=li-dana-meetmail-]").removeClass("active");
+		$(this).parent().siblings("[id^=dana-penawaran-]").show();
+		$(this).parent().siblings("[id^=dana-meetmail-]").hide();
+	});
+	$("[id^=li-dana-meetmail-]").click(function(event) {
+		$(this).addClass("active");
+		$("[id^=li-dana-penawaran-]").removeClass("active");
+		$(this).parent().siblings("[id^=dana-meetmail-]").show();
+		$(this).parent().siblings("[id^=dana-penawaran-]").hide();
+	});
+
+		/* SDM */
+    $("[id^=btn-sdm-]").click(function(event) {
+    	$(this).parent().siblings("[id^=bantu-sdm-]").show();
+    });
+	$("[id^=sdm-meetmail-]").hide();
+	$("[id^=li-sdm-penawaran-]").click(function(event) {
+		$(this).addClass("active");
+		$("[id^=li-sdm-meetmail-]").removeClass("active");
+		$(this).parent().siblings("[id^=sdm-penawaran-]").show();
+		$(this).parent().siblings("[id^=sdm-meetmail-]").hide();
+	});
+	$("[id^=li-sdm-meetmail-]").click(function(event) {
+		$(this).addClass("active");
+		$("[id^=li-sdm-penawaran-]").removeClass("active");
+		$(this).parent().siblings("[id^=sdm-meetmail-]").show();
+		$(this).parent().siblings("[id^=sdm-penawaran-]").hide();
+	});
+
+		/* TEKNO */
+    $("[id^=btn-tekno-]").click(function(event) {
+    	$(this).parent().siblings("[id^=bantu-tekno-]").show();
+    });
+	$("[id^=tekno-meetmail-]").hide();
+	$("[id^=li-tekno-penawaran-]").click(function(event) {
+		$(this).addClass("active");
+		$("[id^=li-tekno-meetmail-]").removeClass("active");
+		$(this).parent().siblings("[id^=tekno-penawaran-]").show();
+		$(this).parent().siblings("[id^=tekno-meetmail-]").hide();
+	});
+	$("[id^=li-tekno-meetmail-]").click(function(event) {
+		$(this).addClass("active");
+		$("[id^=li-tekno-penawaran-]").removeClass("active");
+		$(this).parent().siblings("[id^=tekno-meetmail-]").show();
+		$(this).parent().siblings("[id^=tekno-penawaran-]").hide();
+	});
+
+		/* LAINNYA */
+    $("[id^=btn-lainnya-]").click(function(event) {
+    	$(this).parent().siblings("[id^=bantu-lainnya-]").show();
+    });
+	$("[id^=lainnya-meetmail-]").hide();
+	$("[id^=li-lainnya-penawaran-]").click(function(event) {
+		$(this).addClass("active");
+		$("[id^=li-lainnya-meetmail-]").removeClass("active");
+		$(this).parent().siblings("[id^=lainnya-penawaran-]").show();
+		$(this).parent().siblings("[id^=lainnya-meetmail-]").hide();
+	});
+	$("[id^=li-lainnya-meetmail-]").click(function(event) {
+		$(this).addClass("active");
+		$("[id^=li-lainnya-penawaran-]").removeClass("active");
+		$(this).parent().siblings("[id^=lainnya-meetmail-]").show();
+		$(this).parent().siblings("[id^=lainnya-penawaran-]").hide();
+	});
+    /* END OF FORM PROYEK */
 })
